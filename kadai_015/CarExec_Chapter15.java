92b3365bf1f40aca072a14e0d08ef4d78d3b786b
cjdu <jssdbo1277@yahoo.co.jp>
2024-06-03 23:13:02 +0900

    最初のコミット

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -1,1 +1,11 @@
+public class CarExec_Chapter15 {
+
+  public static void main(String[] args) {
+
+    Car_Chapter15 carA = new Car_Chapter15();
+    carA.gearChange(3);
+    carA.run();
+  }
+}
+
 
