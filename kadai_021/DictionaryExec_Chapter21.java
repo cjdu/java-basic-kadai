74383c1d7aeac097a6c8d3d92e3a465b0a2df2ac
cjdu <jssdbo1277@yahoo.co.jp>
2024-06-15 23:28:27 +0900

    最初のコミット

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -1,1 +1,12 @@
 
+public class DictionaryExec_Chapter21 {
+  
+  public static void main(String[] args) {
+
+    Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+
+    String[] english = new String[] {"apple", "banana", "grape", "orange"};
+
+    dictionary.search (english);
+  }
+}
--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -1,1 +1,50 @@
 
+
+import java.util.HashMap;
+public class Dictionary_Chapter21 {
+
+  public void search(String[]english) {
+
+    HashMap<String,String> dictionary = new HashMap<String,String>();
+
+    String result = "";
+
+    dictionary.put("apple","りんご");
+    dictionary.put("peach", "桃");
+    dictionary.put("banana", "バナナ");
+    dictionary.put("lemon", "レモン");
+    dictionary.put("pear", "梨");
+    dictionary.put("kiwi", "キウイ");
+    dictionary.put("strawberry", "いちご");
+    dictionary.put("grape", "ぶどう"); 
+    dictionary.put("muscat", "マスカット"); 
+    dictionary.put("cherry", "さくらんぼ");
+
+    for (int i = 0; i < english.length; i++) {
+      result = dictionary.get(english[i]);
+
+      if (result == null) {
+        result = english[i] + "は辞書にはありません";
+      } else {
+        result = english[i] + "の意味は" + result + "です";
+      }
+      System.out.println(result);
+    
+    }
+  }
+}   
+    
+    
+    
+      
+
+  
+
+ 
+
+
+
+  
+
+  
+
